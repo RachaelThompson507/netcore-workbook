8de2e71f1a89705071612f1c076ca31941ffd60a
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-16 19:59:43 -0600

    fixed tag helper in view cshtml

---
+++ Lesson02/StartOfLesson/ToDo/TagHelpers/DayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "day-of-the-week")]
+    public class DayOfWeekTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent("Hello from DayOfTheWeek");
+        }
+    }
+}
---
+++ Lesson02/StartOfLesson/ToDo/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ToDoApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "day-of-the-week")]
+    public class EmailTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <email> with <a> tag
+        }
+    }
+}

481c464336135c9e93c8b42b12b0f6ecabd2705f
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-16 21:03:29 -0600

    this added tag helper logic

--- Lesson02/StartOfLesson/ToDo/TagHelpers/DayOfTheWeek.cs
+++ Lesson02/StartOfLesson/ToDo/TagHelpers/DayOfTheWeek.cs
@@ -9,9 +9,14 @@ namespace ToDoApp.TagHelpers
     [HtmlTargetElement("*", Attributes = "day-of-the-week")]
     public class DayOfWeekTagHelper : TagHelper
     {
+        public DateTime TodoDate { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.Content.SetContent("Hello from DayOfTheWeek");
+            // output.Content.SetContent("Hello from DayOfTheWeek");
+
+            //should receive from the tag helper -
+            var stringDate = TodoDate < DateTime.Now.AddDays(7) ? TodoDate.DayOfWeek.ToString() : TodoDate.ToString("MM/dd/yyyy");
+            output.Content.SetContent(stringDate);
         }
     }
 }

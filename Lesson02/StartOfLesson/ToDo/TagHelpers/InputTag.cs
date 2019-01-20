d5c6ded1fe78247613b978be673da126633496f8
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-20 16:22:38 -0600

    Added custom tag helper

---
+++ Lesson02/StartOfLesson/ToDo/TagHelpers/InputTag.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.IdentityModel.Protocols.WsFederation.WsFederationConstants;
+
+namespace ToDoApp.TagHelpers
+{
+    [HtmlTargetElement("input", Attributes = "autocomplete")]
+    public class InputTagHelper: TagHelper
+    {
+        public HtmlAttributeNameAttribute AutoCompleteHelp { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (AutoCompleteHelp.ToString() == "off" || AutoCompleteHelp.ToString() == "false" || AutoCompleteHelp.ToString() == "disabled")
+            {
+                output.Attributes.SetAttribute("AutoCompleteHelp", "off");
+            }
+            else
+            {
+                output.Attributes.SetAttribute("AutoCompleteHelp", "on");
+            }
+        }
+    }
+}
